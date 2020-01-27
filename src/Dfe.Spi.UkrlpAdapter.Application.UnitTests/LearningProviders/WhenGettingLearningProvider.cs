5bd4a498c882e5d4ae3279550cffa973b79fc114
Simon Bull <simon.bull.84@googlemail.com>
2020-01-27 16:08:19 +0000

    Enforce UKPRN length of 8

--- src/Dfe.Spi.UkrlpAdapter.Application.UnitTests/LearningProviders/WhenGettingLearningProvider.cs
+++ src/Dfe.Spi.UkrlpAdapter.Application.UnitTests/LearningProviders/WhenGettingLearningProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoFixture;
 using AutoFixture.NUnit3;
 using Dfe.Spi.Common.Logging.Definitions;
 using Dfe.Spi.Common.UnitTesting.Fixtures;
@@ -15,6 +16,7 @@ namespace Dfe.Spi.UkrlpAdapter.Application.UnitTests.LearningProviders
 {
     public class WhenGettingLearningProvider
     {
+        private Fixture _fixture;
         private Mock<IUkrlpApiClient> _ukrlpApiClientMock;
         private Mock<IMapper> _mapperMock;
         private Mock<ILoggerWrapper> _loggerMock;
@@ -24,10 +26,14 @@ namespace Dfe.Spi.UkrlpAdapter.Application.UnitTests.LearningProviders
         [SetUp]
         public void Arrange()
         {
+            var random = new Random();
+            _fixture = new Fixture();
+            _fixture.Register(() => (long) random.Next(10000000, 99999999));
+
             _ukrlpApiClientMock = new Mock<IUkrlpApiClient>();
 
             _mapperMock = new Mock<IMapper>();
-            
+
             _loggerMock = new Mock<ILoggerWrapper>();
 
             _manager = new LearningProviderManager(_ukrlpApiClientMock.Object, _mapperMock.Object, _loggerMock.Object);
@@ -35,12 +41,14 @@ namespace Dfe.Spi.UkrlpAdapter.Application.UnitTests.LearningProviders
             _cancellationToken = new CancellationToken();
         }
 
-        [Test, AutoData]
-        public async Task ThenItShouldGetProviderFromApi(int urn)
+        [Test]
+        public async Task ThenItShouldGetProviderFromApi()
         {
-            await _manager.GetLearningProviderAsync(urn.ToString(), _cancellationToken);
+            var ukprn = _fixture.Create<long>();
 
-            _ukrlpApiClientMock.Verify(c => c.GetProviderAsync(urn, _cancellationToken),
+            await _manager.GetLearningProviderAsync(ukprn.ToString(), _cancellationToken);
+
+            _ukrlpApiClientMock.Verify(c => c.GetProviderAsync(ukprn, _cancellationToken),
                 Times.Once);
         }
 
@@ -51,38 +59,51 @@ namespace Dfe.Spi.UkrlpAdapter.Application.UnitTests.LearningProviders
                 await _manager.GetLearningProviderAsync("NotANumber", _cancellationToken));
         }
 
-        [Test, AutoData]
-        public async Task ThenItShouldMapProviderToLearningProvider(int urn, Provider establishment)
+        [Test]
+        public void ThenItShouldThrowExceptionIfIdIsNot8Digits()
         {
-            _ukrlpApiClientMock.Setup(c => c.GetProviderAsync(urn, _cancellationToken))
+            Assert.ThrowsAsync<ArgumentException>(async () =>
+                await _manager.GetLearningProviderAsync("123456789", _cancellationToken));
+        }
+
+        [Test, AutoData]
+        public async Task ThenItShouldMapProviderToLearningProvider(Provider establishment)
+        {
+            var ukprn = _fixture.Create<long>();
+            
+            _ukrlpApiClientMock.Setup(c => c.GetProviderAsync(ukprn, _cancellationToken))
                 .ReturnsAsync(establishment);
 
-            await _manager.GetLearningProviderAsync(urn.ToString(), _cancellationToken);
+            await _manager.GetLearningProviderAsync(ukprn.ToString(), _cancellationToken);
 
             _mapperMock.Verify(m => m.MapAsync<LearningProvider>(establishment, _cancellationToken),
                 Times.Once);
         }
 
         [Test, NonRecursiveAutoData]
-        public async Task ThenItShouldReturnMappedLearningProvider(int urn, LearningProvider learningProvider)
+        public async Task ThenItShouldReturnMappedLearningProvider(LearningProvider learningProvider)
         {
-            _ukrlpApiClientMock.Setup(c => c.GetProviderAsync(urn, _cancellationToken))
+            var ukprn = _fixture.Create<long>();
+            
+            _ukrlpApiClientMock.Setup(c => c.GetProviderAsync(ukprn, _cancellationToken))
                 .ReturnsAsync(new Provider());
             _mapperMock.Setup(m => m.MapAsync<LearningProvider>(It.IsAny<Provider>(), _cancellationToken))
                 .ReturnsAsync(learningProvider);
 
-            var actual = await _manager.GetLearningProviderAsync(urn.ToString(), _cancellationToken);
+            var actual = await _manager.GetLearningProviderAsync(ukprn.ToString(), _cancellationToken);
 
             Assert.AreSame(learningProvider, actual);
         }
 
         [Test, AutoData]
-        public async Task ThenItShouldReturnNullIfProviderNotFoundOnApi(int urn)
+        public async Task ThenItShouldReturnNullIfProviderNotFoundOnApi()
         {
-            _ukrlpApiClientMock.Setup(c => c.GetProviderAsync(urn, _cancellationToken))
+            var ukprn = _fixture.Create<long>();
+            
+            _ukrlpApiClientMock.Setup(c => c.GetProviderAsync(ukprn, _cancellationToken))
                 .ReturnsAsync((Provider) null);
 
-            var actual = await _manager.GetLearningProviderAsync(urn.ToString(), _cancellationToken);
+            var actual = await _manager.GetLearningProviderAsync(ukprn.ToString(), _cancellationToken);
 
             Assert.IsNull(actual);
         }
